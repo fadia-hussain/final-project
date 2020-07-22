d1373676b330cbd205d0d24b146548def751b901
unknown <fadiahussain1326@gmail.com>
2020-07-23 00:20:37 +0300

    Another fail at removing repetition
    I am struggling to remove names that have already been used as i am using random.choice

--- python-final-project.py
+++ python-final-project.py
@@ -3,11 +3,22 @@
 
 
 # file = open("groups", "a")
-students = ["Hasan", "John", "Amira", "Salma", "Ashley", "Peter"]
-length = len(students)
-for student in range (0,3):
-    print("Group : ", random.choice(students) + " + " + random.choice(students) )
+import random
+students = ["Hasan", "John", "Amira", "Salma", "Ashley", "Peter","Omar", "Shehnaz","Claudio", "Kaie","Mariam", "Rubel", "Mishika", "Fadia"]
 
+length = len(students)
+
+# number_of_groups = 7
+# while students>0 and number_of_groups>0:
+
+for student in range (0,7):
+    print("Group : ", random.choice(students) + " + " + random.choice(students) )
+    del(students([-1]))
+    print(students)
+
+
+
+# print (students)
 # file.close()
 
 
