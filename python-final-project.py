321040d656264ed962cd9e20e46fc59d90db7659
unknown <fadiahussain1326@gmail.com>
2020-07-22 21:51:09 +0300

    Change in range
    I changed the range from 0 to the whole length (which is 6) to 0-3. I realised that since I was pairing them up, I need the for loop to work thrice only.

--- python-final-project.py
+++ python-final-project.py
@@ -5,7 +5,7 @@
 # file = open("groups", "a")
 students = ["Hasan", "John", "Amira", "Salma", "Ashley", "Peter"]
 length = len(students)
-for student in range (0,length):
+for student in range (0,3):
     print("Group : ", random.choice(students) + " + " + random.choice(students) )
 
 # file.close()
