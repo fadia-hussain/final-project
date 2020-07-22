cc9f60873d6ac707e167fb17073b59f570dda4a9
unknown <fadiahussain1326@gmail.com>
2020-07-22 21:30:13 +0300

    Failed file.write()
    I tried to copy paste the list created to a txt file but from what I can gather, I need to manually type everything I want to see there. Not sure how to automatically transfer the list of groups.

--- python-final-project.py
+++ python-final-project.py
@@ -2,27 +2,14 @@
 # Fadia Hussain
 
 
-file = open("groups", "a")
+# file = open("groups", "a")
 students = ["Hasan", "John", "Amira", "Salma", "Ashley", "Peter"]
 length = len(students)
 for student in range (0,length):
     print("Group : ", random.choice(students) + " + " + random.choice(students) )
-file.write("print")
-file.close()
 
-# for student in students:
-#      print("Group : ", random.choice(students) + " + " + random.choice(students) )
-# num_students = len(students)
-# print(num_students)
+# file.close()
 
 
-# import random
-# humans = ["Fadia", "Hasan", "John", "Lauren", "Ahmed"]
-# print ("The winner is", random.choice (humans), "!")
 
-#
-# import random
-
-#     print("Group : ", random.choice(students) + " + " + random.choice(students) )
-# repeat_7 (7,do)
 
