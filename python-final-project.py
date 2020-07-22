476fd7b4b1ac0eff6eca905867ab9e0698518a7b
unknown <fadiahussain1326@gmail.com>
2020-07-22 21:11:34 +0300

    Use of for loop
    By using for loop, I was able to use all items in the list. However, the names repeated

--- python-final-project.py
+++ python-final-project.py
@@ -1,16 +1,28 @@
 # final-project
 # Fadia Hussain
 
+
+file = open("groups", "a")
 students = ["Hasan", "John", "Amira", "Salma", "Ashley", "Peter"]
-print(students)
+length = len(students)
+for student in range (0,length):
+    print("Group : ", random.choice(students) + " + " + random.choice(students) )
+file.write("print")
+file.close()
 
-num_students = len(students)
-print(num_students)
+# for student in students:
+#      print("Group : ", random.choice(students) + " + " + random.choice(students) )
+# num_students = len(students)
+# print(num_students)
 
-for student in students:
-    print(student)
 
-print ("hello")
-print ("how are you")
-print ("i hope i figured it out")
-print ("works i guess?")
+# import random
+# humans = ["Fadia", "Hasan", "John", "Lauren", "Ahmed"]
+# print ("The winner is", random.choice (humans), "!")
+
+#
+# import random
+
+#     print("Group : ", random.choice(students) + " + " + random.choice(students) )
+# repeat_7 (7,do)
+
