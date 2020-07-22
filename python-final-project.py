909798d65b5f4cc3875073ac04550e6b6e2b43fb
unknown <fadiahussain1326@gmail.com>
2020-07-23 01:12:37 +0300

    Final product
    That's the best I could do :(

--- python-final-project.py
+++ python-final-project.py
@@ -5,14 +5,23 @@ import random
 students = ["Hasan", "John", "Amira", "Salma", "Ashley", "Peter","Omar", "Shehnaz","Claudio", "Kaie","Mariam", "Rubel", "Mishika", "Fadia"]
 
 length = len(students)
+
 print("The number of students in this class is: ",length)
 
-team = random.choice(students)
-print(team)
-del (team)
-# for student in range (0,7):
-#     print("Group : ", random.choice(students) + " + " + random.choice(students) )
-#     del (random.choice(students))
+print("And now they will be separated into groups of two: \n")
+for student in range (0,7):
+
+    print("Group : ", random.choice(students) + " + " + random.choice(students) )
+
+
+# team = random.choice(students) + random.choice(students)
+# print("Group: " + team)
+# del (team)
+
+
+
+
+
 
 
 
